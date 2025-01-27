2561ccecbd05b56f53ee7afa3ae9b166c45d09e1
vic2207 <150955356+vic2207@users.noreply.github.com>
2025-01-27 17:47:01 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        MyArrayList list = new MyArrayList();
+        list.add("Hello");
+        list.add("World");
+        System.out.println(list.get(1));
+    }
+}
